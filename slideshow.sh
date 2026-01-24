9d82857d6d5b3e4422dbf196858b383908474b07
KeithEKelleyJr <32264734+KeithEKelleyJr@users.noreply.github.com>
2026-01-24 17:49:39 -0500

    add slideshow.sh

---
+++ slideshow.sh
@@ -0,0 +1,8 @@
+sudo rm -r /home/br/Pictures/Slides/blackrabbitmedia
+
+cd /home/br/Pictures/Slides
+git clone https://github.com/KeithEKelleyJr/blackrabbitmedia.git
+
+#!/bin/sh
+cd /home/br/Pictures/Slides/blackrabbitmedia
+feh -Z -z -F -D 60 --hide-pointer --auto-rotate
