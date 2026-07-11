e8bea0e6485026bd2f42626810481f29cc091ab0
KeithEKelleyJr <32264734+KeithEKelleyJr@users.noreply.github.com>
2026-07-10 22:07:44 -0400

    update

--- slideshow.sh
+++ slideshow.sh
@@ -5,5 +5,4 @@ git clone https://github.com/KeithEKelleyJr/blackrabbitmedia.git
 
 #!/bin/sh
 cd /home/br/Pictures/Slides/blackrabbitmedia
-#!feh -Z -z -F -D 60 --hide-pointer --auto-rotate
-find . -maxdepth 1 -type f \( -iname '*.jpg' -o -iname '*.jpeg' -o -iname '*.png' -o -iname '*.gif' \) | sort -V | xargs feh -Z -z -F -D 60 --hide-pointer --auto-rotate
+ls -v | feh -Z -z -F -D 60 --hide-pointer --auto-rotate -f -
